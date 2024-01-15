784655e2380750ea93ca18c058b426f3dfd0cf79
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-01-15 16:50:45 -0600

    course 14 - 21

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Post from "./components/Post";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Post author="Cassie" body="React.js is awesome!" />
+      <Post author="Nightingale" body="Check out more course." />
+    </>
+  );
 }
 
 export default App;
