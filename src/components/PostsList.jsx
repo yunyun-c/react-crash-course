98185b3899516112f6f9a50683b8ff3633601c28
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-01-17 23:09:06 -0600

    course 31

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,35 +1,18 @@
-import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 import Modal from "./Modal";
 
 function PostList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
 
       <ul className={styles.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Nightingale" body="Check out more course." />
       </ul>
     </>
