3dce9ef2c0223e2d378404f348a4d6dd71b4eac6
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-01-18 13:51:50 -0600

    course 32 - 37

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,19 +2,59 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 import Modal from "./Modal";
+import { useEffect, useState } from "react";
 
 function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("https://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  function addPostHandler(postsData) {
+    fetch("https://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postsData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setPosts((existingPosts) => [postsData, ...existingPosts]);
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
 
-      <ul className={styles.posts}>
-        <Post author="Nightingale" body="Check out more course." />
-      </ul>
+      {!isFetching && posts.length > 0 && (
+        <ul className={styles.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet!</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Fetching posts...</p>
+        </div>
+      )}
     </>
   );
 }
